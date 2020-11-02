a5ceef3f82e057e2c2eabdbf76e7a5110ac2da6a
KontradZebrowski1 <73643863+KontradZebrowski1@users.noreply.github.com>
2020-11-02 11:22:17 +0100

    Update Wektor2D.hpp

--- zal/Wektor2D.hpp
+++ zal/Wektor2D.hpp
@@ -1,4 +1,51 @@
 class Wektor2D
 {
+  public:
     // Tutaj napisz implementacje klasy Wektor2D
+    Wektor2D()
+    {
+      x = 0;
+      y = 0;
+    }
+    Wektor2D(double a, double b)
+    {
+      x = a;
+      y = b;
+    }
+    void setX(double a)
+    {
+      x = a;
+    }
+    void setY(double b)
+    {
+      y = b;
+    }
+    double getX()
+    {
+      return x;
+    }
+    double getY()
+    {
+      return y;
+    }
+  private:
+  double x;
+  double y;
 };
+Wektor2D operator+(Wektor2D a, Wektor2D b)
+{
+  Wektor2D suma();
+  double xk = a.getX() + b.getX();
+  double yk = a.getY() + b.getY();
+  suma.setX(xk);
+  suma.setY(yk);
+  return suma;
+}
+double operator*(Wektor2D a, Wektor2D b)
+{
+  double iloczyn;
+  double xk = a.getX() * b.getX();
+  double yk = a.getY() * b.getY();
+  iloczyn = xk+yk;
+  return iloczyn;
+}
